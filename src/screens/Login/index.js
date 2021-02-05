112ab78bf749aab62531981557fa72d12763dd46
Soheil Jafarzade <soheil.jafarzade@gmail.com>
2021-02-05 23:40:56 +0330

    add Login screen

---
+++ src/screens/Login/index.js
@@ -0,0 +1,48 @@
+'use strict';
+import type {Node} from 'react';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import {Input, Button} from 'react-native-elements';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+// import ImagePicker from 'react-native-image-picker';
+import ImagePicker from '../../component/ImagePicker/index.js';
+import styles from './styles.js';
+const Login = (props): Node => {
+  const [image, setImage] = useState(null);
+  const handleImagePicker = () => {
+    launchCamera(
+      {
+        mediaType: 'photo',
+        includeBase64: true,
+        // saveToPhotos: true,
+      },
+      (res) => {
+        console.log({res});
+        setImage(res.uri);
+      },
+    );
+  };
+  return (
+    <>
+      <ImagePicker uri={image} />
+      <View style={styles.sectionContainer}>
+        <Input
+          placeholder="Name"
+          errorStyle={{color: 'red'}}
+          // errorMessage="create apk"
+        />
+      </View>
+      <View style={styles.sectionContainer}>
+        <Input placeholder="Family" errorStyle={{color: 'red'}} />
+      </View>
+      <View style={styles.sectionContainer}>
+        <Input placeholder="Phone" errorStyle={{color: 'red'}} />
+      </View>
+      <View style={styles.sectionContainer}>
+        <Button title="Submit" onPress={handleImagePicker}></Button>
+      </View>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/screens/Login/styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 12,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+});
+
+export default styles;
